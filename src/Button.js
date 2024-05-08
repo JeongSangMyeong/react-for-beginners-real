146b974e7942e0d5b6e4dab937eec3e7d0bfb915
정상명 <ajflsp@naver.com>
2024-05-08 11:07:37 +0900

    #5.1 Tour of CRA(css모듈화)

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,16 @@
-import PropType from "prop-types";
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button ({text}) {
-    return <button>{text}</button>
+    return (
+        <button className={styles.btn}>
+            {text}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
 }
 
 export default Button;
