fa6e13d3eeb7a5fcd5df94d386914445a9405217
정상명 <ajflsp@naver.com>
2024-05-08 14:32:05 +0900

    #6.4 Cleanup(useEffect의 return function 지정)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
+import { func } from "prop-types";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => {
-          setKeyword(e.target.value);
-        };
-  useEffect(() => {
-    console.log("I run only once");
+function Hello() {
+  useEffect(function() {
+    console.log("hi :)");
+    return function() {
+      console.log("bye :(");
+    }
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' Changes,");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
-  
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
